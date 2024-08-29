14339b041359cc95bb42ec634149c364050106d8
joesabado <126978788+joesabado@users.noreply.github.com>
2024-08-29 00:51:15 -0700

    Auto-update: Thu 08/29/2024  0:51:15.44

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log('Script version: 2023-05-10-008');
+console.log('Script version: 2023-05-10-010');
 
 const AIRTABLE_API_KEY = 'patbL8p7Pmy3Wpwlh.41d17501ee07102e1d63590b972f73de0736a3db992b5bd9a5f2482a9b666774';
 const AIRTABLE_BASE_ID = 'apphtyz3OAaOMcBM5';
